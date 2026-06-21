b14f825361677c1736927c9aa79e65c9f74a88a6
Sue Eze <sue.eze@outlook.com>
2026-06-21 21:11:06 +0100

    Add Iris prediction endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import joblib
 
 app = Flask(__name__)
 
+model = joblib.load("model.pkl")
+
 @app.route("/")
 def home():
-    return "Hello Brow! Flask is working."
+    return "Iris Prediction API Running"
+
+@app.route("/predict")
+def predict():
+    prediction = model.predict([[5.1, 3.5, 1.4, 0.2]])
+    return f"Prediction: {prediction[0]}"
 
 if __name__ == "__main__":
     app.run(debug=True)
